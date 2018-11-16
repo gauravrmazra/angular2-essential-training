43b04702f01a6492106719b34dbafae75d6d6607
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-16 14:05:15 +0530

    Angular Custom validator example on Model driven form

--- app/media-item-form.component.ts
+++ app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -12,10 +12,27 @@ export class MediaItemFormComponent {
   ngOnInit() {
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      name: new FormControl('', Validators.compose([Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')])),
       category: new FormControl(''),
-      year: new FormControl(''),
+      year: new FormControl('', this.yearValidator),
     });
+  }
+
+  yearValidator(control: FormControl) {
+    if(control.value.trim().length === 0) {
+      return null;
+    }
+
+    let year = parseInt(control.value);
+    let minYear = 1900;
+    let maxYear = 2100;
+
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    }
+    else {
+      return { 'year': true };
+    }
   }
 
   onSubmit(mediaItem) {
