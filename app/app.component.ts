f8f35ab68d6028140ed43bc1467cec4640059ac5
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2018-11-16 11:58:23 +0530

    - Example of custom events - Input and Output passing to and fro component

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core'
     templateUrl: 'app/app.component.html',
     styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+    firstMediaItem =  {
+        id: 1,
+        name: "Firebug",
+        medium: "Series",
+        category: "Science Fiction",
+        year: 2010,
+        watchedOn: 1294166565384,
+        isFavourite: false
+    };
+
+    onMediaItemDelete(mediaItem) {
+        console.log(mediaItem);
+    }
+}
